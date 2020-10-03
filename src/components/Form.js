53375629ed3cfa82d566795997be51b1eeeacc0a
PhillJK <farrux2315@gmail.com>
2020-10-03 17:45:01 +0500

    Completed works with Firebase and added Loader

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,21 @@
 import React, { useState, useContext } from "react";
 import AlertContext from "../context/alert/alertContext";
+import FirebaseContext from "../context/firebase/firebaseContext";
 
 const Form = () => {
   const [value, setValue] = useState("");
   const alert = useContext(AlertContext);
+  const firebase = useContext(FirebaseContext);
 
   const submitHandler = (event) => {
     event.preventDefault();
     if (value.trim()) {
-      alert.show(" The task was created", "success");
+      firebase
+        .addNote(value.trim())
+        .then(() => {
+          alert.show(" The task was created", "success");
+        })
+        .catch(alert.show(" There was some error", "danger"));
       setValue("");
     } else {
       alert.show(" The name of the task must not be empty");
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,22 +1,24 @@
 import React from "react";
 
-const Notes = ({ notes }) => {
-  return (
-    <ul className="list-group">
-      {notes.map((note) => (
-        <li className="list-group-item note" key={note.id}>
-          <div>
-            <strong>{note.title}</strong>
-            <small>{new Date().toLocaleDateString()}</small>
-          </div>
+const Notes = ({ notes, onRemove }) => (
+  <ul className="list-group">
+    {notes.map((note) => (
+      <li className="list-group-item note" key={note.id}>
+        <div>
+          <strong>{note.title}</strong>
+          <small>{note.date}</small>
+        </div>
 
-          <button type="button" className="btn btn-outline-danger btn-sm">
-            delete
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
-};
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm"
+          onClick={() => onRemove(note.id)}
+        >
+          delete
+        </button>
+      </li>
+    ))}
+  </ul>
+);
 
 export default Notes;
