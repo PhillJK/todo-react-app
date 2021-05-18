56d6708e47f24ac891a6b3d5e2629eaea2e05c5b
PhillJK <farrux2315@gmail.com>
2021-05-18 06:31:38 +0500

    Updated packages and Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,12 @@
-import React, { Fragment, useContext, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import Form from "../components/Form";
 import Loader from "../components/Loader";
 import Notes from "../components/Notes";
 import FirebaseContext from "../context/firebase/firebaseContext";
 
 const Home = () => {
-  const { loading, notes, fetchNotes, removeNote } = useContext(
-    FirebaseContext
-  );
+  const { loading, notes, fetchNotes, removeNote } =
+    useContext(FirebaseContext);
 
   useEffect(() => {
     fetchNotes();
@@ -15,12 +14,12 @@ const Home = () => {
   }, []);
 
   return (
-    <Fragment>
+    <>
       <Form />
       <hr />
 
       {loading ? <Loader /> : <Notes notes={notes} onRemove={removeNote} />}
-    </Fragment>
+    </>
   );
 };
 
